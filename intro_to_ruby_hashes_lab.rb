f96d921560997db567764394db70025bba67e130
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-30 14:08:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = {
     :railroads => {
-      :pieces => 4, :rent_in_dollars {}, 
+      :pieces => 4, 
+      :rent_in_dollars {},
+      :names => {}
     }
   }
 end
